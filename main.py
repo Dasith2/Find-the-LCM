6c3e3248fd43c7c4d48fb66b6bc31d0ea3a21605
Dasith2 <135091650+Dasith2@users.noreply.github.com>
2024-04-17 23:08:47 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+def get_elements():
+    n = int(input("Enter number of elements(n): "))
+    return n
 
+def num(a):
+    elements = []
+    z = 1
+    while z <= a:
+        n = int(input("Enter number {}: ".format(z)))
+        elements.append(n)
+        z += 1
+    return elements
+
+def gcd(x, y):
+    while y != 0:
+        x, y = y, x % y
+    return x
+
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
+
+def lcm_of_list(numbers):
+    lcm_value = numbers[0]
+    for i in range(1, len(numbers)):
+        lcm_value = lcm(lcm_value, numbers[i])
+    return lcm_value
+
+def main():
+    number = get_elements()
+    numbers_list = num(number)
+    result = lcm_of_list(numbers_list)
+    print("LCM of the given numbers:", result)
+
+main()
